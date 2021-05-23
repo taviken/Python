98bc4cae23c45f9ea99b3e1925273d9dcd579ac4
taviken <trevor.viken@gmail.com>
2021-05-23 11:24:15 -0500

    Update _crypto.py
    added encrypt/decrypt wrapper functions

--- crypto/_crypto.py
+++ crypto/_crypto.py
@@ -1,11 +1,13 @@
 import cryptography.hazmat.primitives.asymmetric.rsa as rsa
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.asymmetric import padding
-
-_padding = padding.PSS(mgf=padding.MGF1(hashes.SHA256()),
-                       salt_length=padding.PSS.MAX_LENGTH)
-
 from cryptography.exceptions import *
+
+_PSS_padding = padding.PSS(mgf=padding.MGF1(hashes.SHA256()),
+                           salt_length=padding.PSS.MAX_LENGTH)
+
+_OAEP_padding = padding.OAEP(mgf=padding.MGF1(algorithm=hashes.SHA256()), algorithm=hashes.SHA256(), label=None)
+
 
 class Crypt:
     def __init__(self):
@@ -20,7 +22,7 @@ class Crypt:
     def sign(self, message: bytes):
         signature = self._private.sign(
             message,
-            _padding,
+            _PSS_padding,
             hashes.SHA256()
         )
         return signature
@@ -29,7 +31,20 @@ class Crypt:
         pub = self._private.public_key()
         verified = True
         try:
-            pub.verify(signature, message, _padding, hashes.SHA256())
+            pub.verify(signature, message, _PSS_padding, hashes.SHA256())
         except InvalidSignature:
             verified = False
         return verified
+
+    def encrypt(self, message: bytes):
+        pubkey = self._private.public_key()
+        return pubkey.encrypt(
+            message,
+            _OAEP_padding
+        )
+
+    def decrypt(self, ciphertext: bytes):
+        return self._private.decrypt(
+            ciphertext,
+            _OAEP_padding
+        )
