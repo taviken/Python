c47258da7f739c54d96914c0cc0e9660b0ee7b63
taviken <trevor.viken@gmail.com>
2021-07-31 00:30:09 -0500

    update
    interstitial commit

--- bitspec/__init__.py
+++
@@ -1,9 +0,0 @@
-from bitspec._bitspec import (
-    Word,
-    create_word_class,
-)
-
-__all__ = [
-    'Word',
-    'create_word_class',
-]
--- djangofett/django_fett/blog/models.py
+++ djangofett/django_fett/blog/models.py
@@ -5,11 +5,14 @@ from django.contrib.auth.models import User
 
 # Create your models here. (ie database models)
 class Post(models.Model):
+    """
+    Class that represents a blog post, is the python interface to the database. Is part of the backend
+    """
     title = models.CharField(max_length=100)
     content = models.TextField()
     date_posted = models.DateTimeField(default=timezone.now)
     # the author is a 1 to many relation
     author = models.ForeignKey(to=User, on_delete=models.CASCADE)
 
-    def __str__(self):
+    def __str__(self) -> str:
         return f'{self.__class__.__name__}({self.title})'
