fc07f26b7216fb57cb6b8f24cd490c6b8abf02be
taviken <trevor.viken@gmail.com>
2021-08-20 17:27:09 -0500

    Update backend.py

--- metric/backend.py
+++ metric/backend.py
@@ -1,12 +1,18 @@
 import subprocess
 import json
+import os
+import sys
+import multimetric
 
-MULTIMETRIC = 'multimetric'
+_path = os.path.dirname(multimetric.__file__)
+_main = os.path.join(_path, '__main__.py')
+
+# MULTIMETRIC = 'multimetric'
 
 
 def get_analyitics(source_path: str) -> dict:
     """Retrieve the analytics from a given source code path"""
-    pipe = subprocess.Popen((MULTIMETRIC, source_path),
+    pipe = subprocess.Popen((sys.executable, _main, source_path),
                             stdout=subprocess.PIPE,
                             creationflags=subprocess.CREATE_NO_WINDOW)
     result = pipe.communicate()
