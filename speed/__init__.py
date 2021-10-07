e5d17007155a29bc30e3639ea2a368dcad470d64
taviken <trevor.viken@gmail.com>
2021-10-06 21:23:28 -0500

    created simple speed test logger

---
+++ speed/__init__.py
@@ -0,0 +1,6 @@
+from speed.Speed import main,get_download_speed
+
+__all__ = [
+    'main',
+    'get_download_speed',
+]
---
+++ speed/_constants.py
@@ -0,0 +1,7 @@
+import os
+
+kibibyte = 1024
+mibibyte = kibibyte * kibibyte
+
+__dir = os.path.split(__file__)[0]
+__path = os.path.join(__dir, 'downloads.log')
