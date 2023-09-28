a4cfe953a439b09f4eb853ee394a1b9a4c486c39
taviken <trevor.viken@gmail.com>
2023-09-28 03:03:32 +0000

    adding unique module

--- unique/_unique.py
+++ unique/_unique.py
@@ -1,16 +1,21 @@
 """Module that contains recipes for unique classes"""
+
+class UniqueValueError(ValueError):
+    pass
+
 def make_unique(*,key):
-    class Unique(type):
+    """Creates a metaclass with the uniquness pattern based on a given key"""
+    class MetaUnique(type):
         _req_key=key
         _ids=set()
         def __call__(cls,*a,**k):
             inst = super().__call__(*a,**k)
             key_ = getattr(inst,key)
             if key_ in Unique._ids:
-                raise Exception(f"{key_} already taken")
+                raise UniqueValueError(f"{key_} already taken")
             Unique._ids.add(key_)
             return inst
-    return Unique
+    return MetaUnique
 
 class foo(metaclass=make_unique(key='name')):
     def __init__(self, name:str):
