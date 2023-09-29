f94312b3a55f093b4f59a63bb0091cc6b757d49f
taviken <trevor.viken@gmail.com>
2023-09-28 23:08:59 -0500

    Update _unique.py

--- unique/_unique.py
+++ unique/_unique.py
@@ -5,30 +5,6 @@ class UniqueValueError(ValueError):
     """'Uniuq Subclass of ValueError"""
 
 
-# def make_unique(*,key):
-#     """Creates a metaclass with the uniquness pattern based on a given key"""
-#     class MetaUnique(type):
-#         """Unique meta class"""
-#         _req_key=key
-#         _ids=set()
-#         def __call__(cls,*a,**k):
-#             inst = super().__call__(*a,**k)
-#             key_ = getattr(inst,key)
-#             if key_ in MetaUnique._ids:
-#                 raise UniqueValueError(f"{key_} already taken")
-#             MetaUnique._ids.add(key_)
-#             return inst
-
-#         def __del__(cls):
-#             key_ = getattr(cls,key)
-#             if key_ in MetaUnique._ids:
-#                 MetaUnique._ids.discard(cls)
-#     return MetaUnique
-
-# class foo(metaclass=make_unique(key='name')):
-#     def __init__(self, name:str):
-#         self.name = name
-
 def make_unique(*, key):
     """Creates a metaclass with the uniquness pattern based on a given key"""
     class MetaUnique(type):
