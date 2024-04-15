47701677d0455b4456990403d22b3bd903acd059
taviken <trevor.viken@gmail.com>
2024-04-15 18:32:12 -0500

    Update _tests.py

--- peg_parser/_tests.py
+++ peg_parser/_tests.py
@@ -22,20 +22,7 @@ meta_grammar = [
     "alternative: item+",
     "item: NAME | STRING",
 ]
-symbols = {
-    "EQ": r"\=",
-    "Plus": r"\+",
-    "Minus": r"\-",
-    "LeftBrace": "{",
-    "RightBrace": "}",
-    "LeftBracket": r"\[",
-    "RightBracket": r"\]",
-    "ForwardSlash": r"/",
-    "BackSlash": r"\\",
-    "Quote": r"\"",
-    "Start": r"\*",
-    "Colon": r"\:",
-}
+
 rules = {
     "whitespace": " |\t",
     "alpha": "[A-Za-z_]+",
@@ -51,7 +38,6 @@ def test_lexer():
             "unsigned",
             "module",
         ],
-        symbols=symbols,
         rules=rules,
     )
     source = [
@@ -59,29 +45,28 @@ def test_lexer():
         "\tunsigned long var = 42;",
         "};",
     ]
-    lexer = Lexer(source, lexicon)
+    lexer = Lexer.from_str(source, lexicon)
 
     actual_tokens = lexer.tokens
     expected_tokens = [
-        Token(text=" ", kind="whitespace", lineno=0, span=(0, 1)),
-        Token(text="module", kind="module", lineno=0, span=(1, 7)),
-        Token(text=" ", kind="whitespace", lineno=0, span=(7, 8)),
-        Token(text="test", kind="alpha", lineno=0, span=(8, 12)),
-        Token(text="12345", kind="numeric", lineno=0, span=(12, 17)),
-        Token(text=" ", kind="whitespace", lineno=0, span=(17, 18)),
-        Token(text="{", kind="LeftBrace", lineno=0, span=(18, 19)),
-        Token(text=" ", kind="whitespace", lineno=0, span=(19, 20)),
-        Token(text="\t", kind="whitespace", lineno=1, span=(0, 1)),
-        Token(text="unsigned", kind="unsigned", lineno=1, span=(1, 9)),
-        Token(text=" ", kind="whitespace", lineno=1, span=(9, 10)),
-        Token(text="long", kind="long", lineno=1, span=(10, 14)),
-        Token(text=" ", kind="whitespace", lineno=1, span=(14, 15)),
-        Token(text="var", kind="alpha", lineno=1, span=(15, 18)),
-        Token(text=" ", kind="whitespace", lineno=1, span=(18, 19)),
-        Token(text="=", kind="EQ", lineno=1, span=(19, 20)),
-        Token(text=" ", kind="whitespace", lineno=1, span=(20, 21)),
-        Token(text="42", kind="numeric", lineno=1, span=(21, 23)),
-        Token(text="}", kind="RightBrace", lineno=2, span=(0, 1)),
+        Token(text=" ", kind="INDENT", category="INDENT", lineno=1, span=(0, 1)),
+        Token(text="module", kind="MODULE", category="NAME", lineno=1, span=(1, 7)),
+        Token(text="test12345", kind="NAME", category="NAME", lineno=1, span=(8, 17)),
+        Token(text="{", kind="LBRACE", category="OP", lineno=1, span=(18, 19)),
+        Token(text="~", kind="TILDE", category="OP", lineno=1, span=(20, 21)),
+        Token(
+            text="unsigned", kind="UNSIGNED", category="NAME", lineno=1, span=(22, 30)
+        ),
+        Token(text="long", kind="LONG", category="NAME", lineno=1, span=(31, 35)),
+        Token(text="var", kind="NAME", category="NAME", lineno=1, span=(36, 39)),
+        Token(text="=", kind="EQUAL", category="OP", lineno=1, span=(40, 41)),
+        Token(text="42", kind="NUMBER", category="NUMBER", lineno=1, span=(42, 44)),
+        Token(text=";", kind="SEMI", category="OP", lineno=1, span=(44, 45)),
+        Token(text="}", kind="RBRACE", category="OP", lineno=1, span=(45, 46)),
+        Token(text=";", kind="SEMI", category="OP", lineno=1, span=(46, 47)),
+        Token(text="", kind="NEWLINE", category="NEWLINE", lineno=1, span=(47, 48)),
+        Token(text="", kind="DEDENT", category="DEDENT", lineno=2, span=(0, 0)),
+        Token(text="", kind="ENDMARKER", category="ENDMARKER", lineno=2, span=(0, 0)),
     ]
 
     assert expected_tokens == actual_tokens
@@ -96,7 +81,6 @@ def test_parser():
             NAME,
             NEWLINE,
         ],
-        symbols=symbols,
         rules=rules,
     )
     program = [
@@ -104,7 +88,7 @@ def test_parser():
         "asmt: NAME '=' expr\n",
         "expr: NAME\n",
     ]
-    lexer = Lexer(program, lexicon)
+    lexer = Lexer.from_str(program, lexicon)
     parser = GrammarParser(lexer)
     _rules = parser.grammar()
     assert _rules is [
