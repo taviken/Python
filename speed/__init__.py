d62e061ffb426e8ee054be17b3055ab87e4158cd
taviken <trevor.viken@gmail.com>
2024-05-31 15:21:39 -0500

    deleting old code

--- speed/__init__.py
+++
@@ -1,6 +0,0 @@
-from speed._speed import get_download_speed, main
-
-__all__ = [
-    'get_download_speed',
-    'main',
-]
--- speed/_constants.py
+++
@@ -1,7 +0,0 @@
-import os
-
-kibibyte = 1024
-mibibyte = kibibyte * kibibyte
-
-__dir = os.path.split(__file__)[0]
-__path = os.path.join(__dir, 'downloads.log')
