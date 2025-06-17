b6f55267c1b91044fceb71dca0fab7a650125fa3
taviken <trevor.viken@gmail.com>
2025-06-16 21:55:30 -0600

    Update __init__.py

--- BlockChain_old/__init__.py
+++ BlockChain_old/__init__.py
@@ -1,9 +1,6 @@
-from BlockChain.Chains import (
+from BlockChain_old.Chains import (
     Block,
     BlockChain,
 )
 
-__all__ = [
-    'Block',
-    'BlockChain'
-]
+__all__ = ["Block", "BlockChain"]
