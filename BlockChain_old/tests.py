2687d34ecd52a6f7d38d8e434ee9088caed19559
taviken <trevor.viken@gmail.com>
2025-06-16 21:55:33 -0600

    Update tests.py

--- BlockChain_old/tests.py
+++ BlockChain_old/tests.py
@@ -1,5 +1,5 @@
 import unittest
-import BlockChain as bc
+import BlockChain_old as bc
 
 
 class TestBlockChain(unittest.TestCase):
@@ -8,14 +8,14 @@ class TestBlockChain(unittest.TestCase):
         self.chain = bc._BlockChain()
 
     def test_adding_transaction(self):
-        transaction = ['alice gives bob $20']
-        self.chain.add_new_transaction('alice gives bob $20')
+        transaction = ["alice gives bob $20"]
+        self.chain.add_new_transaction("alice gives bob $20")
         self.chain.mine()
         block = self.chain.last_block
         self.assertEqual(transaction, block.transactions)
 
     def test_adding_mulltiple_transactions(self):
-        transactions = ['alice gives bob $30', 'bob gives alice herpes']
+        transactions = ["alice gives bob $30", "bob gives alice herpes"]
         for transaction in transactions:
             self.chain.add_new_transaction(transaction)
         self.chain.mine()
