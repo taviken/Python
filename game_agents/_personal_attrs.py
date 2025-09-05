7555d4eae6e2547439a47f638dd6bdedb0e023cc
taviken <trevor.viken@gmail.com>
2025-09-04 22:14:58 -0600

    Added personality attributes

---
+++ game_agents/_personal_attrs.py
@@ -0,0 +1,16 @@
+class PersonalAtrrs:
+    def __init__(
+        self,
+        strength: int,
+        intelligence: int,
+        dexterity: int,
+        constitution: int,
+        charisma: int,
+        wisdom: int,
+    ) -> None:
+        self.strength: int = strength
+        self.intelligence: int = intelligence
+        self.dexterity: int = dexterity
+        self.constitution: int = constitution
+        self.charisma: int = charisma
+        self.wisdom: int = wisdom
