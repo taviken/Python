83927e1382a507f97b306a11788d476cc47b2aab
taviken <trevor.viken@gmail.com>
2025-09-05 21:50:20 -0600

    Create _vessel.py

---
+++ game_agents/_vessel.py
@@ -0,0 +1,16 @@
+from dataclasses import dataclass, field
+from typing import List
+
+
+@dataclass
+class Cargo:
+    capacity:int
+    contents:List[str]=field(default_factory=list)
+    
+    
+@dataclass
+class Vessel:
+    name:str
+    weapons:List[Weapon]=field(default_factory=list)
+    hold:Cargo
+    hull_integrity=int
