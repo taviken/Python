9bc7069d524be99d7018055065324afc9a4be936
taviken <trevor.viken@gmail.com>
2025-09-05 18:12:10 -0600

    Update _personal_attrs.py
    Changed to dataclass

--- game_agents/_personal_attrs.py
+++ game_agents/_personal_attrs.py
@@ -1,16 +1,12 @@
+from dataclasses import dataclass
+
+
+@dataclass
 class PersonalAtrrs:
-    def __init__(
-        self,
-        strength: int,
-        intelligence: int,
-        dexterity: int,
-        constitution: int,
-        charisma: int,
-        wisdom: int,
-    ) -> None:
-        self.strength: int = strength
-        self.intelligence: int = intelligence
-        self.dexterity: int = dexterity
-        self.constitution: int = constitution
-        self.charisma: int = charisma
-        self.wisdom: int = wisdom
+    strength: int
+    intelligence: int
+    dexterity: int
+    constitution: int
+    charisma: int
+    wisdom: int
+    
