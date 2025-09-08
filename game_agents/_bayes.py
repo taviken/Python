808f17524ecbc2d5507f50093881239425ea61d6
taviken <trevor.viken@gmail.com>
2025-09-07 23:45:04 -0500

    Update _bayes.py

--- game_agents/_bayes.py
+++ game_agents/_bayes.py
@@ -1,12 +1,16 @@
 def bayes(
     p_h: float,  # The prior
     p_eh: float,  # the probability of hypothesis given an event
+    p_enh: float = None,
+    p_nh: float = None,
 ) -> float:
     """
     Runs bayes theorem given the initial probabilty,
     """
-    p_enh = 1 - p_h
-    p_nh = 1 - p_eh
+    if p_enh is None:
+        p_enh = 1 - p_h
+    if p_nh is None:
+        p_nh = 1 - p_eh
     num = p_h * p_eh
     denom = num + (p_nh * p_enh)
 
