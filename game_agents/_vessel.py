737f60111cbdb763e47ec91a2a9a5ed8f5bb2678
taviken <trevor.viken@gmail.com>
2025-09-08 22:28:40 -0600

    Update _vessel.py

--- game_agents/_vessel.py
+++ game_agents/_vessel.py
@@ -4,13 +4,13 @@ from typing import List
 
 @dataclass
 class Cargo:
-    capacity:int
-    contents:List[str]=field(default_factory=list)
-    
-    
+    capacity: int
+    contents: List[str] = field(default_factory=list)
+
+
 @dataclass
 class Vessel:
-    name:str
-    weapons:List[Weapon]=field(default_factory=list)
-    hold:Cargo
-    hull_integrity=int
+    name: str
+    weapons: List[str] = field(default_factory=list)
+    hold: Cargo
+    hull_integrity = int
