256cef2a30d5cbd810d5aa7c73acfbc85d6474bf
taviken <trevor.viken@gmail.com>
2025-09-09 00:20:27 -0500

    Update _name_generator.py

--- game_agents/_name_generator.py
+++ game_agents/_name_generator.py
@@ -1,12 +1,22 @@
-from data import male, female
+from data import male, female, lastnames
 import random
 
 
-def generate_name(gender):
-    if gender == "m":
-        keys = tuple(male.keys())
-        lut = male
-    else:
-        keys = tuple(female.keys())
-        lut = female
+def pick_name(lut):
+    keys = tuple(lut.keys())
     key = random.choice(keys)
+    name_data = lut[key]
+    names, freqs = zip(*name_data)
+    return random.choices(names, freqs)
+
+def generate_name(lname=None):
+    gender = random.choice("m", "f")
+    lut = male if gender == "m" else female
+    fname = pick_name(lut)
+    mname = pick_name(lut)
+    if lname is None:
+        lname = pick_name(lastnames)
+    return fname, mname, lname
+    
+    
+    
