b28eefb53810503a557d285697257435bf1a2139
taviken <trevor.viken@gmail.com>
2025-09-11 13:50:27 -0600

    Minor refactor of generate_name

--- game_agents/_name_generator.py
+++ game_agents/_name_generator.py
@@ -1,4 +1,5 @@
 from data import male, female, lastnames
+from typing import Dict
 import random
 
 
@@ -9,14 +10,14 @@ def pick_name(lut):
     names, freqs = zip(*name_data)
     return random.choices(names, freqs)
 
-def generate_name(lname=None):
-    gender = random.choice("m", "f")
-    lut = male if gender == "m" else female
+
+def generate_name(lut: Dict):
     fname = pick_name(lut)
     mname = pick_name(lut)
     if lname is None:
         lname = pick_name(lastnames)
     return fname, mname, lname
-    
-    
-    
+
+
+def generate_gender():
+    return random.choice("m", "f")
